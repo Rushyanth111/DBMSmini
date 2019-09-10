59fde1873ba7bca87ceb6f06b47db3e5053889b4
RichardDelwin <35023976+RichardDelwin@users.noreply.github.com>
2019-09-10 14:32:34 +0530

    added anganwadi tables, resolved multiple connection errors

--- TableView.py
+++ TableView.py
@@ -4,19 +4,17 @@ from PyQt5.QtWidgets import QTableView
 
 ## Simple Table View.
 class Table(QTableView):
-    def __init__(self, databaseName, TableName, parent=None):
+    def __init__(self, databaseName, TableName, database, parent=None):
         super().__init__(parent=parent)
         self.setEditTriggers(self.NoEditTriggers);
         self.dbName = databaseName
         self.table = TableName
+        self.db = database
         self.createTable()
 
     def createTable(self):
-        db = QSqlDatabase.addDatabase("QSQLITE")
-        db.setDatabaseName(self.dbName)
-        db.open()
 
-        model = QSqlTableModel(self, db)
+        model = QSqlTableModel(self, self.db)
         model.setTable(self.table)
         model.select()
         self.setModel(model)
