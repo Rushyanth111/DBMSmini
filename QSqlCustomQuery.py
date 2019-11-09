6ccea518d23a0ffb7e06f5101a960247ee966cdc
Rushyanth111 <42034084+Rushyanth111@users.noreply.github.com>
2019-11-10 00:23:40 +0530

    QCustomQuery is Complete.

--- QSqlCustomQuery.py
+++ QSqlCustomQuery.py
@@ -1,6 +1,7 @@
 """Holds QCustomQuery -- A replacement for the QSqlQuery Class Internally with convience
 """
 
+from typing import List, Any
 from PyQt5.QtSql import QSqlQuery, QSqlDatabase
 
 
@@ -10,30 +11,49 @@ class QCustomQuery(QSqlQuery):
     Keyword Arguments:
 
         query {str} -- [Query String to be executed] (default: {""})
-
-        db {QSqlDatabase} -- [Database for the Query to be executed on] (default: {QSqlDatabase()})
+        database {QSqlDatabase} -- [Database to be executed on] (default: {QSqlDatabase()})
     """
 
-    def __init__(self, query: str = "", db=QSqlDatabase()):
-        super().__init__(query=query, db=db)
+    def __init__(self, query: str = "", database=QSqlDatabase()):
+        super().__init__(query=query, db=database)
         self.__result__ = []
         self.exec_(query)
-        self.GetAllRecords()
+        self.get_all_records()
 
-    def GetAllRecords(self):
+    def get_all_records(self) -> List[List[Any]]:
+        """Obtains all Records of the Query Exectued.
+
+        Returns:
+
+            [list] -- [list of list of all rows.]
+        """
         while self.next():
             inner_res = []
-            for x in range(self.record().count()):
-                inner_res.append(self.value(x))
+            for itr in range(self.record().count()):
+                inner_res.append(self.value(itr))
             self.__result__.append(inner_res)
         return self.__result__
 
-    def get_row(self, row_num):
-        pass
+    def get_row(self, row_num: int) -> List[Any]:
+        """[summary]
 
-    def get_column(self, col_num):
-        pass
+        Arguments:
 
-    def check_empty_result(self):
-        pass
+            row_num {int} -- [description]
 
+        Returns:
+
+            list -- [description]
+        """
+        return self.__result__[row_num]
+
+    def get_column(self, col_num) -> List[Any]:
+        """[summary]
+
+        Arguments:
+            col_num {[type]} -- [description]
+
+        Returns:
+            list -- [description]
+        """
+        return list(map(list, zip(*self.__result__)))[col_num]
